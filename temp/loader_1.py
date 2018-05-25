e6c057ccb8dd5aa1652b5eecc903ef436f05043d
bosskwei <bosskwei@gmail.com>
2018-05-25 11:52:52 +0800

    impl prior box and fix bugs

--- temp/loader_1.py
+++ temp/loader_1.py
@@ -31,26 +31,26 @@ class VOC2007(Dataset):
         image = io.imread('{0}/{1}/{2:06d}.jpg'.format(self.ROOT, self.IMAGE, item))
 
         # read label
-        objects = []
+        boxes = []
         tree = etree.parse('{0}/{1}/{2:06d}.xml'.format(self.ROOT, self.LABEL, item))
         for obj in tree.getroot().findall('object'):
             name = obj.find('name').text
             box = obj.find('bndbox')
             left, right = int(box.find('xmin').text), int(box.find('xmax').text)
             top, bottom = int(box.find('ymin').text), int(box.find('ymax').text)
-            objects.append([name, left, right, top, bottom])
+            boxes.append([name, left, right, top, bottom])
 
         # return
-        return image, objects
+        return image, boxes
 
 
 def test_voc2007():
     voc2007 = VOC2007()
     #
     fig, ax = plt.subplots()
-    image, objects = voc2007[random.randrange(len(voc2007))]
+    image, boxes = voc2007[random.randrange(len(voc2007))]
     plt.imshow(image)
-    for name, left, right, top, bottom in objects:
+    for name, left, right, top, bottom in boxes:
         color = random.choice(['red', 'green', 'blue'])
         plt.text(left, top, name, color=color)
         rect = patches.Rectangle((left, top), (right - left), (bottom - top), edgecolor=color, linewidth=2, fill=False)
@@ -78,7 +78,7 @@ class VOC2012(Dataset):
     def _parse_label(self, file):
         self.is_not_used()
         # read label
-        objects = []
+        boxes = []
         tree = etree.parse(file)
         filename = tree.getroot().find('filename').text
         for obj in tree.getroot().findall('object'):
@@ -86,8 +86,8 @@ class VOC2012(Dataset):
             box = obj.find('bndbox')
             left, right = float(box.find('xmin').text), float(box.find('xmax').text)
             top, bottom = float(box.find('ymin').text), float(box.find('ymax').text)
-            objects.append([name, left, right, top, bottom])
-        return filename, objects
+            boxes.append([name, left, right, top, bottom])
+        return filename, boxes
 
     def __len__(self):
         return len(self.labels)
@@ -113,9 +113,9 @@ def test_voc2012():
     #
     while True:
         fig, ax = plt.subplots()
-        image, objects = voc2012[random.randrange(len(voc2012))]
+        image, boxes = voc2012[random.randrange(len(voc2012))]
         plt.imshow(image)
-        for name, left, right, top, bottom in objects:
+        for name, left, right, top, bottom in boxes:
             color = random.choice(['red', 'green', 'blue'])
             plt.text(left, top, name, color=color)
             rect = patches.Rectangle((left, top), (right - left), (bottom - top),
@@ -125,7 +125,7 @@ def test_voc2012():
         plt.close()
 
 
-def image_padding(image, target_shape=(512, 512)):
+def image_padding(image, boxes, target_shape=(512, 512)):
     h, w, c = np.shape(image)
     target_h, target_w = target_shape
 
@@ -138,24 +138,28 @@ def image_padding(image, target_shape=(512, 512)):
     offset_h, offset_w = (target_h - h) // 2, (target_w - w) // 2
 
     # fill into target
-    target_image = np.zeros(shape=[target_h, target_w, c], dtype=image.dtype)
+    target_image = np.zeros(shape=[target_h, target_w, c], dtype=np.float32)
     target_image[offset_h:offset_h + h, offset_w:offset_w + w, :] = image
 
+    # adaptive boxes
+    boxes_new = []
+    for name, left, right, top, bottom in boxes:
+        left, right = left * scale + offset_w, right * scale + offset_w
+        top, bottom = top * scale + offset_h, bottom * scale + offset_h
+        boxes_new.append([name, left, right, top, bottom])
+
     #
-    return target_image, scale, offset_w, offset_h
+    return target_image, boxes_new
 
 
 def test_image_padding():
     voc2012 = VOC2012()
     while True:
         fig, ax = plt.subplots()
-        image, objects = voc2012[random.randrange(len(voc2012))]
-        image, scale, offset_x, offset_y = image_padding(image)
+        image, boxes = voc2012[random.randrange(len(voc2012))]
+        image, boxes = image_padding(image, boxes)
         plt.imshow(image)
-        for name, left, right, top, bottom in objects:
-            left, right = left * scale + offset_x, right * scale + offset_x
-            top, bottom = top * scale + offset_y, bottom * scale + offset_y
-            #
+        for name, left, right, top, bottom in boxes:
             color = random.choice(['red', 'green', 'blue'])
             plt.text(left, top, name, color=color)
             rect = patches.Rectangle((left, top), (right - left), (bottom - top),
@@ -165,12 +169,45 @@ def test_image_padding():
         plt.close()
 
 
-def prior_boxes():
-    pass
+def test_prior_boxes():
+    voc2012 = VOC2012()
+    image, boxes = voc2012[random.randrange(len(voc2012))]
+    image, boxes = image_padding(image, boxes)
+    print('num boxes: {}'.format(len(boxes)))
+
+    limit = {0: {'min': 0, 'max': 16},
+             1: {'min': 16, 'max': 32},
+             2: {'min': 32, 'max': 64},
+             3: {'min': 64, 'max': 128},
+             4: {'min': 128, 'max': 256},
+             5: {'min': 256, 'max': 512}}
+    for layer in range(6):
+        # switch plot
+        fig, ax = plt.subplots()
+        ax.imshow(image)
+
+        #
+        scale = pow(2, layer)
+
+        # loop boxes
+        for name, left, right, top, bottom in boxes:
+            height, width = bottom - top, right - left
+            mid_x, mid_y = (left + right) // 2, (top + bottom) // 2
+
+            if limit[layer]['min'] <= max(height, width) < limit[layer]['max']:
+                # prior box in current feature map
+                rect = patches.Rectangle((left // scale, top // scale), width // scale, height // scale,
+                                         edgecolor='red', linewidth=2, fill=False)
+                ax.add_patch(rect)
+
+        #
+        image = transform.pyramid_reduce(image)
+
+    plt.show()
 
 
 def main():
-    pass
+    test_prior_boxes()
 
 
 if __name__ == '__main__':
